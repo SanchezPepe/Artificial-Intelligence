22856f8ca55266c3b83645ae1307034cbd9af155
sanchezpepe <sanchezpepe97@gmail.com>
2019-03-05 12:53:44 -0600

    fichas

---
+++ fichas.pl
@@ -0,0 +1,43 @@
+/*
+
+    Las fichas con igual número de puntos en ambos cuadrados se conocen 
+    como dobles. Así mismo, las fichas con uno de los cuadrados sin 
+    puntos se llaman blancas y las que tienen un punto se conocen como 
+    unos. Así, con los dos, tres, cuatro y cinco hasta llegar a los seis.
+
+*/
+
+ficha(blanca, 0, 0).
+
+ficha(uno, 1, 0).
+ficha(uno, 1, 1).
+
+ficha(dos, 2, 0).
+ficha(dos, 2, 1).
+ficha(dos, 2, 2).
+
+ficha(tres, 3, 0).
+ficha(tres, 3, 1).
+ficha(tres, 3, 2).
+ficha(tres, 3, 3).
+
+ficha(cuatro, 4, 0).
+ficha(cuatro, 4, 1).
+ficha(cuatro, 4, 2).
+ficha(cuatro, 4, 3).
+ficha(cuatro, 4, 4).
+
+ficha(cinco, 5, 0).
+ficha(cinco, 5, 1).
+ficha(cinco, 5, 2).
+ficha(cinco, 5, 3).
+ficha(cinco, 5, 4).
+ficha(cinco, 5, 5).
+
+ficha(seis, 6, 0).
+ficha(seis, 6, 1).
+ficha(seis, 6, 2).
+ficha(seis, 6, 3).
+ficha(seis, 6, 4).
+ficha(seis, 6, 5).
+ficha(seis, 6, 6).
