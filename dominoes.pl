0a5d0b3a395c7d07338bdbe6b4184af73292ef11
sanchezpepe <sanchezpepe97@gmail.com>
2019-03-06 16:54:34 -0600

    pendientes

---
+++ dominoes.pl
@@ -0,0 +1,17 @@
+/**
+Al inicio el sistema recibe:
+    1.- Las fichas asignadas
+    2.- Quién tira primero - quién tuvo la mula más grande
+
+Funciones a implementar:
+    1.- Tomar del pozo (robar)
+    2.- Tirar ficha
+    3.- Tirar ficha rival
+    4.- Función eurística (28-Tiradas-Mías= Fichas del Rival y pozo)
+        a) Cuando el rival tome del pozo, guardar las fichas que no tiene
+        b) Deshacerse lo más rápido posible de las mulas
+        c) Mantener variada la mano de fichas
+    5.- Imprimir tablero
+    
+    
+**/
