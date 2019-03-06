7a7ddac9ed17e23547cac9aa6b3a3a89efe1c565
sanchezpepe <sanchezpepe97@gmail.com>
2019-03-06 16:17:01 -0600

    simplificado

--- fichas.pl
+++ fichas.pl
@@ -1,43 +1,41 @@
 /*
-
     Las fichas con igual número de puntos en ambos cuadrados se conocen 
     como dobles. Así mismo, las fichas con uno de los cuadrados sin 
     puntos se llaman blancas y las que tienen un punto se conocen como 
     unos. Así, con los dos, tres, cuatro y cinco hasta llegar a los seis.
-
 */
 
 ficha(blanca, 0, 0).
 
-ficha(uno, 1, 0).
-ficha(uno, 1, 1).
+ficha(1, 0).
+ficha(1, 1).
 
-ficha(dos, 2, 0).
-ficha(dos, 2, 1).
-ficha(dos, 2, 2).
+ficha(2, 0).
+ficha(2, 1).
+ficha(2, 2).
 
-ficha(tres, 3, 0).
-ficha(tres, 3, 1).
-ficha(tres, 3, 2).
-ficha(tres, 3, 3).
+ficha(3, 0).
+ficha(3, 1).
+ficha(3, 2).
+ficha(3, 3).
 
-ficha(cuatro, 4, 0).
-ficha(cuatro, 4, 1).
-ficha(cuatro, 4, 2).
-ficha(cuatro, 4, 3).
-ficha(cuatro, 4, 4).
+ficha(4, 0).
+ficha(4, 1).
+ficha(4, 2).
+ficha(4, 3).
+ficha(4, 4).
 
-ficha(cinco, 5, 0).
-ficha(cinco, 5, 1).
-ficha(cinco, 5, 2).
-ficha(cinco, 5, 3).
-ficha(cinco, 5, 4).
-ficha(cinco, 5, 5).
+ficha(5, 0).
+ficha(5, 1).
+ficha(5, 2).
+ficha(5, 3).
+ficha(5, 4).
+ficha(5, 5).
 
-ficha(seis, 6, 0).
-ficha(seis, 6, 1).
-ficha(seis, 6, 2).
-ficha(seis, 6, 3).
-ficha(seis, 6, 4).
-ficha(seis, 6, 5).
-ficha(seis, 6, 6).
+ficha(6, 0).
+ficha(6, 1).
+ficha(6, 2).
+ficha(6, 3).
+ficha(6, 4).
+ficha(6, 5).
+ficha(6, 6).
