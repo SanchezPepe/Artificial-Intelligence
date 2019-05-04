5e892259e4dd4167c9adb67fc147b430315d0b1e
sanchezpepe <sanchezpepe97@gmail.com>
2019-05-03 23:45:05 -0500

    java program to index data

--- Movilidad/assets/stationIndex/src/stationindex/StationIndex.java
+++ Movilidad/assets/stationIndex/src/stationindex/StationIndex.java
@@ -37,7 +37,7 @@ public class StationIndex {
         System.out.println("\n");
     }
     
-    public void ordenaEstaciones(String arch) throws FileNotFoundException, IOException{
+    public void ordenaEstaciones(String arch, String sistema) throws FileNotFoundException, IOException{
         File file = new File(arch);
         Scanner sc = new Scanner(file); 
         ArrayList<String[]> stations = new ArrayList<>();
@@ -46,9 +46,8 @@ public class StationIndex {
             stations.add(sc.nextLine().split(",",4)); 
         orden.add(stations.get(0));
         stations.remove(stations.get(0));
-        printA2(orden);
         int i = 0;
-        while(stations.size() > 1 && i < orden.size()){
+        while(stations.size() > 0 && i < orden.size()){
             String pivote[] = orden.get(orden.size()-1);
             String min[] = null;
             double normin = 1000;
@@ -59,7 +58,7 @@ public class StationIndex {
                 double x2 = Double.parseDouble(stations.get(j)[1]);
                 double y2 = Double.parseDouble(stations.get(j)[2]);
                 norma = StationIndex.norm(x1,y1,x2,y2);
-                System.out.println("Norma de:" + pivote[0] + " y " + stations.get(j)[0] + " " +norma);
+                //System.out.println("Norma de:" + pivote[0] + " y " + stations.get(j)[0] + " " +norma);
 
                 if (norma < normin){
                     normin = norma;
@@ -70,15 +69,48 @@ public class StationIndex {
             orden.add(min);
             stations.remove(min);
             i++;
-            System.out.println("=============================");
+            //System.out.println("=============================");
         }
-        printA2(orden);
-
+        //printA2(orden);
+        String res = imprimeIndice(sistema, orden);
+        System.out.println(res);
+    }
+    
+    public String imprimeIndice(String sistema, ArrayList orden){
+        String res = "";
+        for (int i = 0; i < orden.size(); i++) {
+            res += sistema + '(';
+            for (String s : (String[])orden.get(i)) {
+                res += s + ',';
+            }
+            res += (i + 1) + ").\n";
+        }
+        return res;
     }
     
     public static void main(String[] args) throws IOException {
         StationIndex s = new StationIndex();
-        s.ordenaEstaciones("L1.txt");
+        /**
+         * Metrobus
+        System.out.println("%Linea 1");
+        s.ordenaEstaciones("L1.txt", "mb");
+        System.out.println("%Linea 2");
+        s.ordenaEstaciones("L2.txt", "mb");
+        System.out.println("%Linea 3");
+        s.ordenaEstaciones("L3.txt", "mb");
+        System.out.println("%Linea 4");
+        s.ordenaEstaciones("L4.txt", "mb");
+        System.out.println("%Linea 5");
+        s.ordenaEstaciones("L5.txt", "mb");
+        System.out.println("%Linea 6");
+        s.ordenaEstaciones("L6.txt", "mb");
+        *
+        */
+
+        System.out.println("%Linea 1");
+        s.ordenaEstaciones("L1.txt", "metro");
+
+
 
     }
 
